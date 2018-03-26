1df60c1f3dab5b926a98b75a6f70ee1083fa8e46
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-03-26 11:54:18 -0500

    Finished lesson 123.

---
+++ GoalPost/GoalPost/Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  GoalPost
+//
+//  Created by Chetwyn on 3/26/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import Foundation
+
+let goalCell = "goalCell"
---
+++ GoalPost/GoalPost/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Chetwyn on 3/26/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+
+}
