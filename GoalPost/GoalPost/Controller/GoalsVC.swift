20ee48e6e25890055027ea21d59198b2908f2dc2
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-03-28 14:13:20 -0500

    Finished lesson 132,

--- GoalPost/GoalPost/Controller/GoalsVC.swift
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -15,6 +15,8 @@ class GoalsVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var goals: [Goal] = []
+    
     // MARK: - View Configuration
     
     override func viewDidLoad() {
@@ -25,6 +27,26 @@ class GoalsVC: UIViewController {
         tableView.isHidden = false
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.fetch { (successfulFetch) in
+            if successfulFetch {
+                if goals.count >= 1 {
+                    tableView.isHidden = false
+                } else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: - Functions
+    
+    
+    
     // MARK: - Actions
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
@@ -34,6 +56,8 @@ class GoalsVC: UIViewController {
     
 }
 
+// MARK: - TableView Implementation
+
 extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -41,13 +65,39 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: goalCell) as? GoalCell else { return UITableViewCell() }
-        cell.configureCell(description: "Eat salad twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        
+        let goal = goals[indexPath.row]
+        
+        cell.configureCell(goal: goal)
+        
         return cell
     }
+}
+
+// MARK: - Core Data Implementation
+
+extension GoalsVC {
+    
+    func fetch(completion: (_ complete: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
+        
+        do {
+            goals = try managedContext.fetch(fetchRequest) 
+            print("Successfully fetched data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not fetch: \(error.localizedDescription)")
+            completion(false)
+        }
+    }
+    
 }
 
